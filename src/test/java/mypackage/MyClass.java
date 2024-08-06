9309646f829589eaaa95ac3b5351f70f6b1d4843
yuvrajgadadare <42262087+yuvrajgadadare@users.noreply.github.com>
2024-08-06 08:22:09 +0530

    Initial commit

---
+++ src/test/java/mypackage/MyClass.java
@@ -0,0 +1,16 @@
+package mypackage;
+
+public class MyClass {
+
+	public void Display() {
+		System.out.println("Welcome");
+	}
+	public static void main(String[] args) {
+	 
+		MyClass m=new MyClass();
+		m.Display();
+
+	}
+
+
+}
